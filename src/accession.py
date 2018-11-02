092c5910385653261916efd527079e4ce008ebf1
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2018-11-02 12:04:41 -0700

    store blobs locally

--- src/accession.py
+++ src/accession.py
@@ -522,10 +522,7 @@ def filter_outputs_by_path(path):
                 in list(google_backend.bucket.list_blobs())
                 if path in file.id]
     for file in filtered:
-        file.download_to_filename(file.id.split('/')[-1])
-    # with open('filtered.txt', 'a') as f:
-    #     for file in filtered:
-    #         f.write("{}\n".format(file.id))
+        file.download_to_filename(file.public_url.split('/')[-1])
 
 
 if __name__ == '__main__':
